e00fd99bfba78e3e8133c350cf7c62e6dc166a96
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2021-11-03 21:18:42 +0200

    Add jackson-dataformat-csv dependencies. First try create JSON to CSV

--- src/main/java/com/logic/Main.java
+++ src/main/java/com/logic/Main.java
@@ -1,10 +1,17 @@
 package com.logic;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema.Builder;
+
+
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws URISyntaxException, IOException {
@@ -17,6 +24,28 @@ public class Main {
         knownWords.forEach(currentWordInLoop -> {
             var result = freeDictionaryConnector.getWordDescription(currentWordInLoop);
             words.addAll(result);
+        });
+
+        words.forEach(currentListOfWordsLoop -> {
+            try {
+                JsonNode jsonTree = new ObjectMapper().readTree(String.valueOf(words));
+
+                Builder csvSchemaBuilder = CsvSchema.builder();
+                JsonNode firstObject = jsonTree.elements().next();
+                firstObject.fieldNames().forEachRemaining(fieldName -> {csvSchemaBuilder.addColumn(fieldName);} );
+                CsvSchema csvSchema = csvSchemaBuilder.build().withHeader();
+
+                CsvMapper csvMapper = new CsvMapper();
+                csvMapper.writerFor(JsonNode.class)
+                        .with(csvSchema)
+                        .writeValue(new File("result.csv"), jsonTree);
+
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         });
 
         System.out.println(words.size());
